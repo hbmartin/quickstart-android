7a489c21ad05c18ed07378e88d67b65b3a299982
Harold Martin <harold.martin@gmail.com>
2017-05-22 18:51:44 -0700

    wip - load script - firebase

--- load_firebase.py
+++ load_firebase.py
@@ -5,6 +5,7 @@
 __author__ = "H. Martin"
 __version__ = "0.1.0"
 
+import json
 import math
 from random import randint
 import pyrebase
@@ -13,20 +14,8 @@ email = ""
 password = ""
 data = {}
 
-with open('data.json', encoding='utf-8') as data_file:
-    data = json.loads(data_file.read())
-
-for geohash6 in data:
-    geohash_info = {}
-    count = math.ceil(data[geohash6] * 0.5)
-    geohash_info['count'] = count
-    geohash_info['ids'] = {}
-    for i = 1 to count:
-        new_id = str(randint(0, 922337203685477580))
-        geohash_info[new_id] = True
-
 firebase_config = {}
-with open('sub9-client/app/google-services.json ', encoding='utf-8') as data_file:
+with open('sub9-client/app/google-services.json', encoding='utf-8') as data_file:
     firebase_config = json.loads(data_file.read())
 pyrebase_config = {
     "apiKey": firebase_config["client"][0]["api_key"][0]["current_key"],
@@ -34,19 +23,36 @@ pyrebase_config = {
     "databaseURL": firebase_config["project_info"]["firebase_url"],
     "storageBucket": firebase_config["project_info"]["storage_bucket"]
 }
+print(pyrebase_config)
 firebase = pyrebase.initialize_app(pyrebase_config)
 auth = firebase.auth()
 auth.create_user_with_email_and_password(email, password)
 db = firebase.database()
 
-geo4 = db.child(geohash6[4:])
-count4 = geo4.child("count")
-if not count4.get():
-    count4.put(0)
-geo5 = geo4.child(geohash6[5:])
-count5 = geo5.child("count")
-if not count5.get():
-    count5.put(0)
-geo5.child(geohash6).put(geohash_info)
-count5.increment(count)
-count4.increment(count)
+with open('geohash_counter_6.json', encoding='utf-8') as data_file:
+    data = json.loads(data_file.read())
+
+for geohash6 in data:
+    print(geohash6)
+    geohash_info = {}
+    count = math.ceil(data[geohash6] * 0.5)
+    geohash_info['count'] = count
+    geohash_info['ids'] = {}
+    i = 0
+    while i < count:
+        i += 1
+        new_id = str(randint(0, 922337203685477580))
+        geohash_info[new_id] = True
+
+    geo4 = db.child(geohash6[4:])
+    count4 = geo4.child("count")
+    if not count4.get():
+        count4.put(0)
+    geo5 = geo4.child(geohash6[5:])
+    count5 = geo5.child("count")
+    if not count5.get():
+        count5.put(0)
+    geo5.child(geohash6).put(geohash_info)
+    count5.increment(count)
+    count4.increment(count)
+    break
